9c64c35c0a7e13116a18fee79130a9a43fd39ff0
Geek-O-Byte <mmiwf16@gmail.com>
2023-05-11 00:56:00 +0300

    first edit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return(
+    <div>
+        Blog page 
+        <br/>
+        <a href="/">Back to home</a>
+    </div>)
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+    <nav className="sticky top-0 z-50">
+        <ul className='flex justify-center my-6'>
+          <li><a href='/' className='p-3'>Home</a></li>
+          <li><a href='/blog' className='p-3'>Blog</a></li>
+          <li><a href='/' className='p-3'>About</a></li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Navbar;
