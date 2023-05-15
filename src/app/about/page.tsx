44df61733aa99c16db0bd276fcacc0dcf3d3eadd
Geek-O-Byte <mmiwf16@gmail.com>
2023-05-15 12:22:49 +0300

    adding something

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,10 @@ import profilePic from '../public/images/me.jpg'
 export default function Page(){
     return(
     <div> 
-        <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center max-w-xl">
             <img src={'/me.jpg'} className="rounded-full" width={300}/>
             <p className="text-2xl">Max Maslov</p>
         </div>
+        <p>Education</p>
     </div>)
 }
---
+++ src/app/splitbill/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+    return(
+    <div>
+        <p className="text-center text-3xl">temporarily empty, but wait a little bit👽</p>
+    </div>)
+}
