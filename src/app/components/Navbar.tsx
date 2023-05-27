2b29b26acb73f9c5285de804aabf643bb5ea9aff
Geek-O-Byte <mmiwf16@gmail.com>
2023-05-27 19:18:02 +0300

    fix navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
     return(
-    <nav className="sticky top-0 left-0 z-50 bg-white">
-        <ul className='flex justify-center my-6'>
-          <li><a href='/' className='p-3'>Home</a></li>
-          <li><a href='/blog' className='p-3'>Blog</a></li>
-          <li><a href='/about' className='p-3'>About</a></li>
+      <nav className="sticky top-0 left-0 z-50 bg-white h-16 flex items-center">
+        <ul className="flex justify-center w-full">
+          <li className="mx-4"><a href="/" className="px-3 py-2 text-gray-800 hover:text-gray-600">Home</a></li>
+          <li className="mx-4"><a href="/blog" className="px-3 py-2 text-gray-800 hover:text-gray-600">Blog</a></li>
+          <li className="mx-4"><a href="/about" className="px-3 py-2 text-gray-800 hover:text-gray-600">About</a></li>
         </ul>
       </nav>
     )
