40fd1eb20793e36683225f8d3c855822e6c8c547
maxim.maslov <maxim.maslov@csoftcom.com>
2025-12-01 23:08:25 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="vite-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
